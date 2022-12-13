1416e6e8f55640bd16e9190df47da5faf63e11ae
Ammi2324 <abebeleykun@gmail.com>
2022-12-12 19:17:39 -0800

    Lower case in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowecase alphabets in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alr;
+
+	for (alr = 'z'; alr <= 'a'; alr--)
+		putchar(alr);
+
+		putchar('\n');
+
+		return (0);
+}
