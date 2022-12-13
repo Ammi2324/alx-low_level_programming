2ce496870c3c4f3008e283fb49c078f6bb347c27
Ammi2324 <abebeleykun@gmail.com>
2022-12-13 00:27:28 -0800

    0-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints possible two digit combinations
+ *
+ * Return: Always 0 (Success)
+ */
+	int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 58; num++)
+	{
+		putchar(num);
+	if (num != 57)
+		putchar(',');
+		putchar(' ');
+	}
+		return (0);
+}
