eb92c269b6d6d3e3f8ac460794f9d1c3f1012bd3
Ammi2324 <abebeleykun@gmail.com>
2022-12-13 01:03:18 -0800

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 	for (num = 48; num < 58; num++)
 	{
 		putchar(num);
-	if (num != 57)
-	{
-	putchar(',');
-	putchar(' ');
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
-	}
 	return (0);
 }
